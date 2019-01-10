b74aec716be7c4f2d6888ba9f0542fffbf194cd7
bigMzc <mzc19971103@163.com>
2019-01-10 17:45:32 +0800

    eureka server 2+client 2+config+zuul

--- eureka-provider2/src/main/java/com/baizhi/EurekaServerApplication.java
+++ eureka-provider2/src/main/java/com/baizhi/EurekaServerApplication.java
@@ -3,13 +3,8 @@ package com.baizhi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-//支持zookeeper为注册中心
 @EnableDiscoveryClient
-
-//只支持Eureka为注册中心  @EnableEurekaClient
 @SpringBootApplication
 public class EurekaServerApplication {
 
