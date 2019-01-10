2e722156fcc6be4f33f9a4b3563fbc1e786385cd
bigMzc <mzc19971103@163.com>
2019-01-10 14:36:00 +0800

    config center ok

--- config/src/main/java/com/baizhi/EurekaServerApplication.java
+++ config/src/main/java/com/baizhi/EurekaServerApplication.java
@@ -2,12 +2,11 @@ package com.baizhi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-
-@EnableDiscoveryClient
+@EnableConfigServer
+@EnableEurekaClient
 @SpringBootApplication
 public class EurekaServerApplication {
 
